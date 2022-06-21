82be6ec3445611f583734247479d3552df585f81
Stefan J <stefanj@avalantec.com>
2022-06-21 03:15:28 +0000

    updated

--- inteco/models/__init__.py
+++ inteco/models/__init__.py
@@ -5,4 +5,4 @@
 from . import res_users, sector, prefix, res_company, base_import, account_invoice, res_partner, ir_attachment, \
     ir_model_fields, res_config_settings, phone_validation_mixin, helpdesk_ticket, helpdesk_ticket_type, event, \
     organism, committee, ics, product, crm_stage, crm_lead, mail_activity, crm_subservice, crm_team, sale_order, \
-    sale_order_line, website_blog, sale_quote
+    sale_order_line, website_blog, sale_quote, account_move_line
