a5080da220ae196b186f3828b4dffdbf8cbd15cb
Stefan J <stefanj@avalantec.com>
2022-06-24 22:16:40 +0000

    updated

--- inteco/models/.ipynb_checkpoints/account_move_line-checkpoint.py
+++ inteco/models/.ipynb_checkpoints/account_move_line-checkpoint.py
@@ -2,6 +2,8 @@
 
 from odoo import api, fields, models
 
+import logging
+_logging = _logger = logging.getLogger(__name__)
 
 class AccountInvoiceLine(models.Model):
     _inherit = 'account.move.line'
@@ -10,6 +12,9 @@ class AccountInvoiceLine(models.Model):
     def _onchange_product_id(self):
         for record in self:
             if record.product_id:
+                record.product_uom_id = record.product_id.uom_id
+                record.name = record.product_id.name
+                record.price_unit = record.product_id.lst_price
                 if record.product_id.product_tmpl_id.product_analytic_account:
                     an_account = record.product_id.product_tmpl_id.product_analytic_account
                     record.analytic_account_id = an_account
